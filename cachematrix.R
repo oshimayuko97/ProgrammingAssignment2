31f9b7cdcc239b46d0f2e41f66e451d35fcffb06
oshimayuko97 <oshimayuko97@outlook.com>
2018-03-20 23:47:02 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## In the following functions, 
+## a list of an original matrix and its cached matrix is created.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  set <- x
+  get <- x
+  matrices <- list(set = set, get = get)
 }
+matrices <- makeCacheMatrix(x)
 
 
-## Write a short comment describing this function
+## Using the list created above, I firstly check whether the two matrices
+## are identical(if so, inversion has not occurred). If not inversed, the
+## matrix will be inversed and returned to the user.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrices) {
+  m <- matrices$get
+  if(identical(matrices$set,matrices$get)) {
+    message("getting cached data")
+    return(solve(m))
+  }else{
+    return(m)
+  }
 }
